c0574d3f8167fa2a0b0640e0c46db2ba8288343c
Bob Jansen <bobjansen@gmail.com>
2016-11-19 14:46:53 +0100

    Simplify and enable all tests

--- inst/unitTests/runit.AllFormats.R
+++ inst/unitTests/runit.AllFormats.R
@@ -11,45 +11,32 @@ asUTCInUTC <- function(x) {
     utctime(x = x, tz = "UTC")
 }
 
+checkDates <- function(secs, millis) {
+    checkEquals(asUTCInUTC(c(secs, millis)),
+                c(defaultTestDate, defaultTestDateMilliSecs))
+}
 
 test.FormatDataAndTimeUTCNumbers <- function() {
-    checkEquals(asUTCInUTC(c("2016-09-01 10:11:12", "2016-09-01 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("2016/09/01 10:11:12", "2016/09/01 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    #checkEquals(asUTCInUTC(c("20160901 101112",     "20160901 101112.345678")),
-    #            c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("20160901 10:11:12",   "20160901 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("09/01/2016 10:11:12", "09/01/2016 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("09-01-2016 10:11:12", "09-01-2016 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
+    checkDates("2016-09-01 10:11:12", "2016-09-01 10:11:12.345678")
+    checkDates("2016/09/01 10:11:12", "2016/09/01 10:11:12.345678")
+    checkDates("20160901 101112",     "20160901 101112.345678")
+    checkDates("20160901 10:11:12",   "20160901 10:11:12.345678")
+    checkDates("09/01/2016 10:11:12", "09/01/2016 10:11:12.345678")
+    checkDates("09-01-2016 10:11:12", "09-01-2016 10:11:12.345678")
 
 }
 
 test.FormatDataAndTimeUTCAlpha3 <- function() {
-    checkEquals(asUTCInUTC(c("2016-Sep-01 10:11:12", "2016-Sep-01 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("2016/Sep/01 10:11:12", "2016/Sep/01 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("2016Sep01 101112",     "2016Sep01 101112.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("2016Sep01 10:11:12",   "2016Sep01 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("Sep/01/2016 10:11:12", "Sep/01/2016 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("Sep-01-2016 10:11:12", "Sep-01-2016 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("01.Sep.2016 10:11:12", "01.Sep.2016 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("01Sep2016 101112",     "01Sep2016 101112.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("01Sep2016 10:11:12",   "01Sep2016 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("01-Sep-2016 101112",   "01-Sep-2016 101112.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
-    checkEquals(asUTCInUTC(c("01-Sep-2016 10:11:12", "01-Sep-2016 10:11:12.345678")),
-                c(defaultTestDate, defaultTestDateMilliSecs))
+    checkDates("2016-Sep-01 10:11:12", "2016-Sep-01 10:11:12.345678")
+    checkDates("2016/Sep/01 10:11:12", "2016/Sep/01 10:11:12.345678")
+    checkDates("2016Sep01 101112",     "2016Sep01 101112.345678")
+    checkDates("2016Sep01 10:11:12",   "2016Sep01 10:11:12.345678")
+    checkDates("Sep/01/2016 10:11:12", "Sep/01/2016 10:11:12.345678")
+    checkDates("Sep-01-2016 10:11:12", "Sep-01-2016 10:11:12.345678")
+    checkDates("01.Sep.2016 10:11:12", "01.Sep.2016 10:11:12.345678")
+    checkDates("01Sep2016 101112",     "01Sep2016 101112.345678")
+    checkDates("01Sep2016 10:11:12",   "01Sep2016 10:11:12.345678")
+    checkDates("01-Sep-2016 101112",   "01-Sep-2016 101112.345678")
+    checkDates("01-Sep-2016 10:11:12", "01-Sep-2016 10:11:12.345678")
 }
 
